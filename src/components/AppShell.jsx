e329a5c90798680f5063e4ecdc778ae541c47f25
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2022-03-28 02:22:47 +0800

    feat: Migrate to TailwindCSS

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -1,24 +1,16 @@
-import "bootstrap/dist/css/bootstrap.css"
+import "../styles/global.css"
 import "@fontsource/montserrat"
 import "@fontsource/orbitron"
 import "@fontsource/ubuntu"
-import { Helmet } from "react-helmet"
 import Navbar from "./Navbar"
 
 function AppShell({ children }) {
   return (
-    <div className="shell">
-      <Helmet>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossorigin="anonymous"
-        />
-      </Helmet>
+    <>
       <Navbar />
 
-      <div className="container px-0">{children}</div>
-    </div>
+      {children}
+    </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,21 @@ import AppShell from "../components/AppShell"
 function HomePage() {
   return (
     <AppShell>
-      <section>
-        <StaticImage
-          src="https://anztheme.com/assets/anztheme.com/code-ab55892e9e976025b2001a3301f8f07f88b2e8131a5b50142da74d96081cd7e8.jpg"
-          imgClassName="img-fluid"
-          alt="Hero Image"
-        />
-      </section>
+      <main className="container max-w-6xl">
+        <section className="relative">
+          <StaticImage
+            src="https://anztheme.com/assets/anztheme.com/code-ab55892e9e976025b2001a3301f8f07f88b2e8131a5b50142da74d96081cd7e8.jpg"
+            alt="Hero Image"
+            className="h-[60vh] max-h-60 w-full md:max-h-80 xl:max-h-[25rem]"
+          />
 
-      <section></section>
+          <div className="absolute right-4 -bottom-12 flex h-16 w-16 items-center justify-center rounded-full bg-[#0e6fd7] p-12 xl:right-8 xl:-bottom-24 xl:h-44 xl:w-44">
+            <p className="font-montserrat rotate-[20deg] text-center text-xs font-bold text-white xl:text-2xl">
+              Welcome, <br /> ka-CpE!
+            </p>
+          </div>
+        </section>
+      </main>
     </AppShell>
   )
 }
